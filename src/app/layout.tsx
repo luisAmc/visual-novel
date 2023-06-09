3820cde6fa99fcca671b9cd08c3413880584215f
Luis Martinez <luis.kosm@gmail.com>
2023-06-09 00:40:10 -0600

    Updates font to use Chiller

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,27 @@
-import './globals.css';
-import { Inter } from 'next/font/google';
+// import './globals.css';
+// import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+// const inter = Inter({ subsets: ['latin'] });
+
+// export default function RootLayout({
+//   children
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang='en'>
+//       <body className={inter.className}>{children}</body>
+//     </html>
+//   );
+// }
+
+import './globals.css';
+import localFont from 'next/font/local';
+
+const chillerFont = localFont({
+  src: './chiller.ttf',
+  display: 'swap'
+});
 
 export default function RootLayout({
   children
@@ -10,7 +30,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={chillerFont.className}>{children}</body>
     </html>
   );
 }
