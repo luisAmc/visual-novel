5bc565f6481a73a4d7166faef8b0af570339239e
Luis Martinez <luis.kosm@gmail.com>
2023-06-30 23:58:06 -0600

    Adds `CreepyText` a chiller variant to `Text`

--- src/components/Stories/ThePackageStory/CreepyText.tsx
+++ src/components/Stories/ThePackageStory/CreepyText.tsx
@@ -1,13 +1,14 @@
-import { Action } from '.';
+import clsx from 'clsx';
 import { motion } from 'framer-motion';
+import { CHILLER_FONT } from '~/app/layout';
+import { Action } from '~/components/Action';
 
 interface TextProps {
-  src?: string;
   speaker?: string;
   children: string;
 }
 
-export function Text({ children, speaker, src }: TextProps) {
+export function CreepyText({ children, speaker }: TextProps) {
   return (
     <Action name='Text'>
       {(controls) => (
@@ -28,25 +29,22 @@ export function Text({ children, speaker, src }: TextProps) {
               }
             }
           }}
-          className='pointer-events-none select-none absolute inset-0'
-        >
-          {src && (
-            <img
-              src={src}
-              alt='background'
-              className='shadow-md shadow-gray-700'
-            />
+          className={clsx(
+            CHILLER_FONT.className,
+            'text-red-500 pointer-events-none select-none absolute inset-0'
           )}
-
-          <div className='absolute inset-x-12 bottom-12 h-52 rounded-md bg-slate-800/80'>
-            <div className='px-12 py-8 text-5xl leading-snug'>{children}</div>
+        >
+          <div className='absolute inset-x-12 bottom-12 h-52 rounded-md bg-gray-700/95'>
+            <div className='px-12 py-8 text-6xl leading-snug tracking-widest'>
+              {children}
+            </div>
             <div className='absolute text-xl animate-pulse right-12 bottom-12'>
               ▶
             </div>
           </div>
 
           {speaker && (
-            <div className='absolute left-16 bottom-64 px-3 py-2 rounded-md bg-slate-800/90 text-4xl'>
+            <div className='absolute left-16 bottom-64 px-3 py-2 rounded-md bg-slate-800/90 tracking-widest uppercase text-5xl'>
               {speaker}
             </div>
           )}
