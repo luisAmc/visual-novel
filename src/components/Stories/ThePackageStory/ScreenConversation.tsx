e4935a3ab8b8d7847022a0fe388354e63912785a
Luis Martinez <luis.kosm@gmail.com>
2023-06-30 23:56:44 -0600

    Adds `ScreenConversation` component

--- src/components/Stories/ThePackageStory/ScreenConversation.tsx
+++ src/components/Stories/ThePackageStory/ScreenConversation.tsx
@@ -1,14 +1,21 @@
 import { motion } from 'framer-motion';
+import { Fragment } from 'react';
 import { Action } from '~/components/Action';
 
+export type MessageType = {
+  speaker: 'a' | 'b';
+  text: string;
+};
+
 interface ScreenConversationProps {
-  messages: Array<{
-    speaker: 'a' | 'b';
-    text: string;
-  }>;
+  pastMessages: Array<MessageType>;
+  newMessages: Array<MessageType>;
 }
 
-export function ScreenConversation({ messages }: ScreenConversationProps) {
+export function ScreenConversation({
+  pastMessages,
+  newMessages
+}: ScreenConversationProps) {
   return (
     <Action name='ScreenText'>
       {(controls) => (
@@ -31,10 +38,10 @@ export function ScreenConversation({ messages }: ScreenConversationProps) {
           }}
           className='absolute inset-0 h-full flex items-center justify-center bg-slate-800/80'
         >
-          <div className='relative rounded-xl bg-gray-800/90 p-8 w-full max-w-[80%] h-[80%] overflow-auto flex flex-col-reverse gap-y-4'>
-            {[...messages].reverse().map((msg, index) => (
-              <>
-                {index === 1 && (
+          <div className='relative rounded-xl bg-gray-800/90 p-8 w-full max-w-[60%] h-[80%] overflow-auto flex flex-col-reverse gap-y-4'>
+            {[...pastMessages, ...newMessages].reverse().map((msg, index) => (
+              <Fragment key={`msg-${index}`}>
+                {index === newMessages.length && (
                   <div className='w-full border-b text-center text-lg'>
                     <span className='transparent px-3 tracking-widest uppercase leading-10'>
                       Nuevos mensajes
@@ -43,7 +50,6 @@ export function ScreenConversation({ messages }: ScreenConversationProps) {
                 )}
 
                 <motion.div
-                  key={`msg-${index}`}
                   className='rounded-xl bg-gray-700 p-4 max-w-md text-4xl'
                   style={{
                     marginLeft: msg.speaker === 'a' ? 0 : 'auto',
@@ -52,7 +58,7 @@ export function ScreenConversation({ messages }: ScreenConversationProps) {
                 >
                   {msg.text}
                 </motion.div>
-              </>
+              </Fragment>
             ))}
 
             <div className='w-full border-b text-center text-lg'>
