c786e6ab438d4ea5ae077454da797351d26239f3
Luis Martinez <luis.kosm@gmail.com>
2023-06-30 23:56:22 -0600

    Adds `BlackScreen` component

--- src/components/Stories/ThePackageStory/BlackScreen.tsx
+++ src/components/Stories/ThePackageStory/BlackScreen.tsx
@@ -1,24 +1,14 @@
-import { Action } from '.';
 import { motion } from 'framer-motion';
+import { Action } from '~/components/Action';
 
-interface BackgroundProps {
-  src: string;
-  durationMs?: number;
-  instant?: boolean;
-}
-
-export function Background({
-  src,
-  durationMs = 500,
-  instant = false
-}: BackgroundProps) {
+export function BlackScreen({ durationMs = 500 }: { durationMs?: number }) {
   return (
     <Action
-      name='Background'
+      name='BlackScreen'
       statementType={{ variation: 'skippable_timed', durationMs }}
       until={(statement) =>
         statement.actionName === 'Background' ||
-        statement.actionName === 'BlackScreen'
+        statement.actionName === 'PointClick'
       }
     >
       {(controls) => (
@@ -29,7 +19,7 @@ export function Background({
             initial: { opacity: 0 },
             entrance: {
               opacity: 1,
-              transition: { duration: instant ? 0.1 : 1 }
+              transition: { duration: 1 }
             },
             exit: {
               opacity: 0,
@@ -41,7 +31,7 @@ export function Background({
             }
           }}
         >
-          <img src={src} alt='background' className='h-full aspect-video' />
+          <div className='h-full bg-gray-900 aspect-video'></div>
         </motion.div>
       )}
     </Action>
